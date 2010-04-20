1d29afd4242411d9f8927d75bea87fba43c582c3
Jon-Carlos Rivera <imbcmdth@hotmail.com>
2010-04-19 21:40:28 -0400

    added ntree support. created addPlane, addSphere, and addCube functions.

--- js/flog/scene.js
+++ js/flog/scene.js
@@ -7,17 +7,28 @@ Flog.RayTracer.Scene = Class.create();
 Flog.RayTracer.Scene.prototype = {
     camera : null,
     shapes : [],
-    lights : [],
+    planes : [], // Just "too big" to fit into ntree 
+    lights : [], 
     background : null,
-    
+    ntree : null,  
+    addSphere : function(sphere) {
+        this.shapes.push(sphere);
+        var aabb = sphere.bb();
+        this.ntree.insert({intervals:aabb, object:sphere});
+    },
+    addPlane : function(plane) {
+        this.planes.push(plane);
+    },
+    addCube : function(cube) { // TODO
+//        this.shapes.push(plane);
+    },
     initialize : function() {
         this.camera = new Flog.RayTracer.Camera(
             new Flog.RayTracer.Vector(0,0,-5), 
             new Flog.RayTracer.Vector(0,0,1), 
             new Flog.RayTracer.Vector(0,1,0)
         );
-        this.shapes = new Array();
-        this.lights = new Array();
+        this.ntree = new NTree(3, 10); // 3 dimensions, 10 items to a leaf
         this.background = new Flog.RayTracer.Background(new Flog.RayTracer.Color(0,0,0.5), 0.2);
     }
 }
